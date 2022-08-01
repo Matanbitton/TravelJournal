7600f85295cbd34af2f5c12a257fb7c45cb37ee3
Matan <matanbitton2@gmail.com>
2022-08-01 22:19:40 +0300

    make data arr for compnenets to use

---
+++ src/data.js
@@ -0,0 +1,30 @@
+import Australia from "./images/Australia.jpg";
+import Thailand from "./images/Thailand.jpg";
+import LosAngeles from "./images/LosAngeles.jpg";
+
+export const data = [
+  {
+    id: "1",
+    location: "Australia",
+    url: Australia,
+    startDate: "12 Jan, 2021",
+    endDate: "1 August, 2021",
+    description: "Gold Coast in Australia",
+  },
+  {
+    id: "2",
+    location: "Thailand",
+    url: Thailand,
+    startDate: "12 December, 2018",
+    endDate: "1 January, 2018",
+    description: "in Thailand the tropical views are endless",
+  },
+  {
+    id: "3",
+    location: "Los Angeles",
+    url: LosAngeles,
+    startDate: "12 December, 2018",
+    endDate: "1 January, 2018",
+    description: "Golden Sunsets on Malibu beach",
+  },
+];
